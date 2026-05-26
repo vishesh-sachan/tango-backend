f8b1c5153c6f77aad2b9e3121f27016f2d04516b
VISHESH SACHAN <visheshsachan021@gmail.com>
2026-05-26 15:43:56 +0530

    feat: add cors

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ const PORT = 3000;
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(204);
+    }
+
+    next();
+});
+
 app.get('/', (req, res) => {
     res.send('all ok!');
 });
